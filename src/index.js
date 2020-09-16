4a7dc6727b6ae266a44ec0b800b4c9362b9ca3a9
chan <thechanmoon@gmail.com>
2020-09-16 01:14:33 +0000

    make it better

--- src/index.js
+++ src/index.js
@@ -14,12 +14,7 @@ const reducer = (state = [], action) => {
 		case ADD_TODO:
 			return [{ text: action.text, id: Date.now() }, ...state]
 		case DELETE_TODO:
-			// return state.filter((todo) => {
-			// 	console.log('DELETE+TODO : ', todo.id, action.id)
-			// 	return todo.id.toString() !== action.id
-			// })
 			return state.filter((todo) => todo.id !== parseInt(action.id))
-		// return state.filter((todo) => todo.id.toString() !== action.id)
 		default:
 			return state
 	}
@@ -33,9 +28,6 @@ const renderDodoList = () => {
 		let li = document.createElement('li')
 		let button = document.createElement('button')
 		li.innerText = element.text
-		// li.id = 'li-' + index
-		// console.log('li : ' + li)
-		// console.log('li : ' + li.id)
 		button.innerText = 'DEL'
 		button.id = element.id
 		button.addEventListener('click', removeButtonHandler)
@@ -72,38 +64,8 @@ const addButtonHandler = (e) => {
 
 const removeButtonHandler = (e) => {
 	e.preventDefault()
-	// console.log('removeButtonHandler', e, e.target, e.target.id)
-	// // console.log('removeButtonHandler', )
-	// const li = document.getElementById('li-' + e.target.id)
-	// console.log(li)
-	// e.target.remove()
-	// li.remove()
 	dispatchDeleteTodo(e.target.id)
 }
 
 form.addEventListener('submit', addButtonHandler)
 console.log(store)
-// const add = document.getElementById('add')
-// const minus = document.getElementById('minus')
-// const number = document.querySelector('span')
-
-// let count = 0
-
-// number.innerText = count
-
-// const updateText = () => {
-// 	number.innerText = count
-// }
-
-// const handleAdd = () => {
-// 	// console.log('handleadd')
-// 	count++
-// 	updateText()
-// }
-// const handleMinus = () => {
-// 	count--
-// 	updateText()
-// }
-
-// add.addEventListener('click', handleAdd)
-// minus.addEventListener('click', handleMinus)
