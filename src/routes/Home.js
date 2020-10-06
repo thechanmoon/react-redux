e65def259efbb74d7d59db726a52ebf7075bb43e
chanmoonpark <thechanmoon@gmail.com>
2020-10-05 23:34:36 -0500

    mapStateToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,9 @@
 // export default () => "Home";
 import React, {useState} from 'react';
-
-function Home()
+import {connect} from 'react-redux';
+function Home({toDos})
 {
+    // console.log(props);
     const [text, setText] = useState("");
     const onChange = (e) => { setText(e.target.value) }
     const onSubmit = (e) => { e.preventDefault(); console.log(text) ; setText("")}
@@ -13,8 +14,20 @@ function Home()
                 <input type = "text" value = {text} onChange={onChange} />
                 <button>Add</button>
             </form>
+            <ul>{JSON.stringify(toDos)}</ul>
         </>
     )
 } 
 
-export default Home;
+function mapStateToProps(state){
+    // console.log(state);
+    // console.log(ownProps);
+    // return { tiburon: 'special' };
+    return {toDos: state};
+}
+
+function mapDispatchToProps(dispatch){
+    console.log(dispatch)
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home);
--- src/store.js
+++ src/store.js
@@ -17,7 +17,7 @@ export const deleteToDo = id => {
     }
 }
 
-const reducer = (state = [], action) => {
+const reducer = (state = [{tiburon: 'special'}], action) => {
     switch (action.type){
         case ADD:
             return [{text: action.text, id:Date.now()},...state];
